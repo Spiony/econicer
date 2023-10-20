3c17a49917930db97a8d43bbe74115670133cda0
Spiony <marcus.wiens@gmx.de>
2023-10-21 00:47:24 +0200

    Add flow section; Formatting

--- econicer/report.py
+++ econicer/report.py
@@ -1,8 +1,14 @@
 import pylatex as tex
+from itertools import islice
 
 
-class ReportDocument():
+def chunks(data, size):
+    it = iter(data)
+    for _ in range(0, len(data), size):
+        yield {k: data[k] for k in islice(it, size)}
 
+
+class ReportDocument:
     def __init__(self, name, number, bank):
         self.name = name
         self.number = number
@@ -18,21 +24,23 @@ class ReportDocument():
         self.doc = tex.Document(
             f"AccountReport_{self.name}",
             documentclass="article",
-            document_options=['10pt', "a4paper"],
+            document_options=["10pt", "a4paper"],
             geometry_options=geometry_options,
-            lmodern=False
+            lmodern=False,
         )
-        self.doc.preamble.append(tex.NoEscape(
-            r"\renewcommand{\familydefault}{\sfdefault}"))
-        self.doc.preamble.append(tex.Command('usepackage', 'helvet'))
-        self.doc.preamble.append(tex.Command(
-            'usepackage', arguments='placeins', options="section"))
+        self.doc.preamble.append(
+            tex.NoEscape(r"\renewcommand{\familydefault}{\sfdefault}")
+        )
+        self.doc.preamble.append(tex.Command("usepackage", "helvet"))
+        self.doc.preamble.append(
+            tex.Command("usepackage", arguments="placeins", options="section")
+        )
 
         self.addHeader()
 
-        self.doc.preamble.append(tex.Command('title', "Financial Report"))
+        self.doc.preamble.append(tex.Command("title", "Financial Report"))
         # self.doc.preamble.append(Command('bank', 'Anonymous author'))
-        self.doc.preamble.append(tex.Command('date', tex.NoEscape(r'\today')))
+        self.doc.preamble.append(tex.Command("date", tex.NoEscape(r"\today")))
         # \usepackage[section]{placeins}
 
     def generatePDF(self):
@@ -40,8 +48,7 @@ class ReportDocument():
 
     def addHeader(self):
         # Add document header
-        header = tex.PageStyle(
-            "header", header_thickness=1, footer_thickness=1)
+        header = tex.PageStyle("header", header_thickness=1, footer_thickness=1)
         # Create left header
         with header.create(tex.Head("L")):
             header.append(f"Account name: {self.name}")
@@ -64,9 +71,9 @@ class ReportDocument():
         self.doc.preamble.append(header)
         self.doc.change_document_style("header")
 
-    def createFig(self, imagePath, caption, width=tex.NoEscape(r'0.5\linewidth')):
-        fig = tex.SubFigure(position='b', width=width)
-        fig.add_image(str(imagePath), width=tex.NoEscape(r'\linewidth'))
+    def createFig(self, imagePath, caption, width=tex.NoEscape(r"0.5\linewidth")):
+        fig = tex.SubFigure(position="b", width=width)
+        fig.add_image(str(imagePath), width=tex.NoEscape(r"\linewidth"))
         fig.add_caption(caption)
 
         return fig
@@ -90,16 +97,12 @@ class ReportDocument():
         self.doc.append(tex.Command("newpage"))
 
     def addSection(self, title, text="", plotDict={}, plotPaths={}):
-
         with self.doc.create(tex.Section(title)):
-
             if text:
                 self.doc.append(text)
 
-            with self.doc.create(tex.Figure(position='h!')) as fig:
-
+            with self.doc.create(tex.Figure(position="h!")) as fig:
                 for i, (plotName, cap) in enumerate(plotDict.items()):
-
                     subplot = self.createFig(plotPaths[plotName], cap)
                     fig.append(subplot)
 
@@ -107,7 +110,6 @@ class ReportDocument():
                         self.doc.append(tex.LineBreak())
 
     def addYearlyReports(self, plotPaths):
-
         for i, (year, paths) in enumerate(plotPaths.items()):
             self.addYearSection(year, paths)
 
@@ -125,6 +127,31 @@ class ReportDocument():
         }
 
         self.addSection(title, plotDict=plots, plotPaths=plotPaths)
+
+    def addFlowSection(self, plotPaths):
+        """Define plots for the yearly section"""
+
+        title = "Category Flow"
+
+        plots = {k: f"Spending history of '{k}'" for k in plotPaths.keys()}
+
+        # self.addSection(title, plotDict=plots, plotPaths=plotPaths)
+        newpage = False
+        with self.doc.create(tex.Section(title)):
+            for subset in chunks(plots, 6):
+                with self.doc.create(tex.Figure(position="h!")) as fig:
+                    for i, (plotName, cap) in enumerate(subset.items()):
+                        if newpage:
+                            self.doc.append(tex.Command("ContinuedFloat"))
+                            newpage = False
+
+                        subplot = self.createFig(plotPaths[plotName], cap)
+                        fig.append(subplot)
+
+                        if (i + 1) % 2 == 0:
+                            self.doc.append(tex.LineBreak())
+
+                newpage = True
 
     def addStatisticsSection(self, statistics):
         pass
